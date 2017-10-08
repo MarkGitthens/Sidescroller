f8e617d442036b63e990d1ab05700f98ee46eb98
Mark Gitthens <mgitthens@gmail.com>
2017-10-08 17:10:00 -0500

    Added a few operations to Vector2D.

--- Sidescroller/Vector2D.cpp
+++ Sidescroller/Vector2D.cpp
@@ -18,6 +18,27 @@ Vector2D::Vector2D(const Vector2D& copy) {
     this->y = copy.y;
 }
 
+Vector2D Vector2D::normalize() {
+    Vector2D temp(x, y);
+
+    double vectorLength = sqrt(temp.x * temp.x + temp.y * temp.y);
+    temp.x /= vectorLength;
+    temp.y /= vectorLength;
+    
+    return temp;
+}
+
+int Vector2D::dot(Vector2D rhs) {
+    return x*rhs.x + y*rhs.y;
+}
+
+Vector2D Vector2D::project(Vector2D line) {
+    Vector2D projection;
+    projection.x = (dot(line) / (line.x * line.x + line.y*line.y)) * line.x;
+    projection.x = (dot(line) / (line.x * line.x + line.y*line.y)) * line.y;
+
+    return projection;
+}
 Vector2D Vector2D::operator+(const Vector2D& right) {
     this->x += right.x;
     this->y += right.y;
--- Sidescroller/Vector2D.h
+++ Sidescroller/Vector2D.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <math.h>
 //Need to implement operator overloading
 class Vector2D {
 public:
@@ -7,6 +8,10 @@ public:
     Vector2D(const Vector2D&);
     Vector2D(int, int);
     ~Vector2D();
+
+    int dot(Vector2D rhs);
+    Vector2D normalize();
+    Vector2D project(Vector2D line);
     Vector2D operator+(const Vector2D& right);
     Vector2D operator-(const Vector2D& right);
     Vector2D operator*(int scalar);
