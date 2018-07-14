20d84b110b8a20b272cf799206dd7774cf24d12c
MaelstromDrift <mgitthens@gmail.com>
2018-07-13 19:13:38 -0500

    Tabbing it up.

--- src/core/Event.h
+++ src/core/Event.h
@@ -5,7 +5,7 @@ using std::string;
 namespace Vulture2D {
     //Approach for generating ID's take from Oxygine framework.
     constexpr unsigned int getUniqueID(unsigned int a, unsigned int b, unsigned int c, unsigned int d) { return (((unsigned int)a) | ((unsigned int)b << 8) | ((unsigned int)c << 16) | ((unsigned int)d << 24)); }
-constexpr unsigned int getUniqueSystemID(unsigned int a, unsigned int b, unsigned int c) { return getUniqueID(0x0, a, b, c); }
+    constexpr unsigned int getUniqueSystemID(unsigned int a, unsigned int b, unsigned int c) { return getUniqueID(0x0, a, b, c); }
 
     typedef unsigned int EventType;
 
