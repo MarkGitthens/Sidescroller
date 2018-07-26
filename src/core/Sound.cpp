1abe2651c3950ef4ef3389695a6cf331b207af36
MaelstromDrift <mgitthens@gmail.com>
2018-07-26 01:17:02 -0500

    Created files for the AssetPool and started working on the implementation for playing audio.

---
+++ src/core/Sound.cpp
@@ -0,0 +1,41 @@
+#include "Sound.h"
+
+namespace Vulture2D {
+    Sound::~Sound() {
+        destroySound();
+    }
+
+    const SDL_AudioSpec& Sound::getAudioInformation() {
+        return soundInformation;
+    }
+
+    bool Sound::loadFromFile(string path) {
+        bool loaded = true;
+        if(SDL_LoadWAV(path.c_str(), &soundInformation, &soundData, &soundLength) == NULL)
+            loaded = false;
+
+        soundInformation.callback = NULL;
+        soundInformation.userdata = NULL;
+        return  loaded;
+    }
+
+    Uint32 Sound::getLength() {
+        return soundLength;
+    }
+
+    void Sound::destroySound() {
+        SDL_FreeWAV(soundData);
+    }
+
+    void Sound::play(SDL_AudioDeviceID deviceID) {
+
+    }
+
+    void Sound::pause() {
+
+    }
+
+    void Sound::stop() {
+
+    }
+}
