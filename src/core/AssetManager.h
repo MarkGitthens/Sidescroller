c8beb3bfc60712feebf2716ad6cd03c1256a31cd
MaelstromDrift <mgitthens@gmail.com>
2018-07-27 18:39:16 -0500

    Test fix for Unix builds.

--- src/core/AssetManager.h
+++ src/core/AssetManager.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <unordered_map>
+#include <SDL_Mixer.h>
 #include "Texture.h"
+#include "Sound.h"
 
 namespace Vulture2D {
     using std::unordered_map;
@@ -11,11 +13,16 @@ namespace Vulture2D {
         static AssetManager& getInstance();
 
         Texture* createTexture(string path, string name, SDL_Renderer*);
+        Mix_Chunk* createSound(string path, string name, bool loops);
         void registerTexture(Texture* texture, string name);
+        void registerSound(Mix_Chunk* sound, string name);
         Texture* getTexture(string name);
+        Mix_Chunk* getSound(string name);
+
 
     private:
         unordered_map<string, Texture*> textures;
+        unordered_map<string, Mix_Chunk*> sounds;
 
         static AssetManager* instance;
 
